d99a220889ebd34274528e37e1af360a650a06cf
butterchoke <rohanrwa@gmail.com>
2022-07-22 14:49:59 +0400

    created lex.c

---
+++ lex.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void conversion(){
+
+  // HEADER: #INCLUDE RELEVANT LIBS
+  //
+  // INTERPRET:  MAIN() => int main()
+  //             PRINT("SOMETHING"); => printf("%s",content);
+  //             IF = IF
+  //             ELSE = ELSE
+  //             WHILE = WHILE
+  //             FOR = FOR
+
+  
+}
