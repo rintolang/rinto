cca0957c6835bb783750a5077b1198a58820b282
butterchoke <rohanrwa@gmail.com>
2022-07-22 13:25:48 +0400

    Compiler reads file from arguements

---
+++ compiler.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv){
+
+  char ch;
+  FILE *fp = fopen(argv[1], "r");
+  if (fp == NULL) {
+    perror("Error while opening the file.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  while ((ch = fgetc(fp)) != EOF)
+    printf("%c", ch);
+
+}
