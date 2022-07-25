bf00f37fd59cbecc224a46c0ceff7637d8b773bd
butterchoke <rohanrwa@gmail.com>
2022-07-25 11:09:24 +0400

    created parser.c

---
+++ parser.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  char header[] = "#include <stdio.h>\n";
+  // - READ IN TOKEN BUFFER
+  // - REFERENCE TOKEN DICTIONARY
+  // - GENERATE RELEVANT C CODE
+}
