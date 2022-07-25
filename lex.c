c4acbb2b581d2a5318cb4b92a2f7fff781099c85
butterchoke <rohanrwa@gmail.com>
2022-07-25 11:09:24 +0400

    Minor edits, and updates

--- lex.c
+++ lex.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void conversion(){
-
-  // HEADER: #INCLUDE RELEVANT LIBS
-  //
+int tokenGenerator(){
   // INTERPRET:  MAIN() => int main()
   //             PRINT("SOMETHING"); => printf("%s",content);
   //             IF = IF
   //             ELSE = ELSE
   //             WHILE = WHILE
   //             FOR = FOR
+  // IF TEXT HAS (), GENERATE FUNCTION TOKEN;
+  //     IF NAME IS MAIN, MAKE MAIN FUNCTION
+  //
+  // IF KEYWORD 'create void/dataType', GENERATE FUNCTION
+  //
+  // ELSE REFERENCE FUNCTION NAME
 
-  
 }
